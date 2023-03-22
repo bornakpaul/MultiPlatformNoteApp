ecee2b7accb7570bae5c78d926d88c03cf23e6ef
bornakpaul <bornakpaul@gmail.com>
2023-03-22 20:59:55 +0530

    Common business logic created

---
+++ shared/src/commonMain/kotlin/com/example/multiplatformnoteapp/domain/note/SearchNotes.kt
@@ -0,0 +1,16 @@
+package com.example.multiplatformnoteapp.domain.note
+
+import com.example.multiplatformnoteapp.domain.time.DateTimeUtil
+
+class SearchNotes {
+    fun execute(notes: List<Note>, query: String): List<Note> {
+        if (query.isBlank()) return notes
+
+        return notes.filter { note ->
+            note.title.trim().lowercase().contains(query.lowercase()) || note.content.trim()
+                .lowercase().contains(query.lowercase())
+        }.sortedBy { note ->
+            DateTimeUtil.toEpochMillis(note.created)
+        }
+    }
+}
